50e183b9c93496ad533b5c1732310a8d990c601d
Denis <salad40@gmail.com>
2020-08-13 14:08:46 +0300

    env host name fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
