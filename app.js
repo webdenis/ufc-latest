c1394db3223b8e41f30e7e34d923bcf327398d68
Denis <salad40@gmail.com>
2020-08-13 23:32:41 +0300

    test jsdom

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const PORT = process.env.PORT || 3000;
 
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  
+  
+  JSDOM.fromURL("https://www.ufc.com/events", options).then(dom => {
+	console.log(dom.serialize());
+  });
+
   res.end('Hello World');
 });
 
