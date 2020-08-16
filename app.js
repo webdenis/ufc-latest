7b3a633511b9b93f13fa82ea3508e57bab594f8f
Denis <salad40@gmail.com>
2020-08-16 17:52:51 +0300

    latest

--- app.js
+++ app.js
@@ -1,22 +1,52 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 const http = require('http');
 
-const hostname = '0.0.0.0';
+const hostname = 'localhost';
 const PORT = process.env.PORT || 3000;
+
+
+import pkg from 'node-html-parser';
+const { parse } = pkg;
 
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+
+// Settings
+const nextFightsNum = 5; // Number of next fights to generate
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   
-  console.log('Starting JSDOM');
-  JSDOM.fromURL("https://www.ufc.com/events", { runScripts: 'dangerously' }).then(dom => {
-	console.log('Parsing DOM...')
-	console.log(dom.serialize());
+  JSDOM.fromURL("https://en.wikipedia.org/wiki/List_of_UFC_events").then(dom => {
+	
+	var root = parse(dom.serialize());
+	
+	var tables = root.querySelectorAll('.wikitable');
+	
+	// Get latest past fight
+	var latestFight = tables[1].querySelectorAll('tbody')[0].querySelectorAll('tr')[1].toString();
+	
+	// Get next fights
+	let children = tables[0].querySelectorAll('tbody')[0].childNodes;
+	
+	var nextFights = '';
+
+	for (let i = children.length; i >= children.length - nextFightsNum * 2; i--) {
+		if (children[i] && children[i].toString().length > 10) {
+			nextFights += children[i].toString();
+		}
+	}
+	
+	var output = '<h4>Latest event:</h4><table>' + latestFight + '</table><h4>Upcoming events:</h4><table>' + nextFights + '</table>';
+	
+	res.end(output);
   });
 
-  res.end('Hello World');
+  //res.end('Hello World');
+
 });
 
 server.listen(PORT, hostname, () => {
